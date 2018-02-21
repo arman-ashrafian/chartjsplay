b1faf1690165c0618342d3d39c6f940f4ae0e319
Arman Ashrafian <ashy63@gmail.com>
2018-02-20 20:45:02 -0800

    initial

---
+++ main.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    var ctx = document.getElementById("chart").getContext('2d')
+
+    var lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [0,1,2,3,4,5],
+            datasets: [{
+                label: "label",
+                backgroundColor: "rgb(0,100,200)",
+                borderColor: "rgb(0,100,200)",
+                fill: false,
+                data: [{
+                    x: 0,
+                    y: 0
+                }, {
+                    x: 1,
+                    y: 1
+                }, {
+                    x: 2,
+                    y: 4
+                }, {
+                    x: 3,
+                    y: 9
+                }]
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    })
+}
